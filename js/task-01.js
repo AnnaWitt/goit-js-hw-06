d8fe650f57e1eb17e78c5208d69885d89b03cbc0
Anna Witt <110746108+AnnaWitt@users.noreply.github.com>
2023-04-24 00:25:13 +0200

    update

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
-/* eslint-disable no-restricted-syntax */
-const itemsElements = document.querySelectorAll('.item');
-console.log(`Number of categories: ${itemsElements.length}`);
+const items = document.querySelectorAll('.item');
+console.log(`Number of categories: ${items.length}`);
 
-for (const item of itemsElements) {
+// eslint-disable-next-line no-restricted-syntax
+for (const item of items) {
   console.log(`Category: ${item.firstElementChild.textContent}`);
   const elementNumber = item.querySelectorAll('li');
   console.log(`Elements: ${elementNumber.length}`);
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const numberEl = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement('div');
+    divEl.style.width = `${size}px`;
+    divEl.style.height = `${size}px`;
+    size += 10;
+    divEl.style.backgroundColor = getRandomHexColor();
+    boxesEl.append(divEl);
+  }
+}
+createBtn.addEventListener('click', () => {
+  const amount = Number(document.querySelector('#controls input').value);
+  createBoxes(amount);
+});
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+  numberEl.value = '';
+}
+destroyBtn.addEventListener('click', destroyBoxes);
